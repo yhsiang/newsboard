d72bd01412cd54e20e73cff141f3148659ff1194
LY Cheng <lyforever62@hotmail.com>
2016-09-14 15:48:25 +0800

    Use promise in cna

--- src/crawler/cna.js
+++ src/crawler/cna.js
@@ -1,5 +1,6 @@
 import FeedParser from "feedparser";
 import request from "request";
+import Promise from "bluebird";
 import { createJob } from "../job";
 
 export const baseURL = "http://feeds.feedburner.com";
@@ -21,46 +22,57 @@ export const URLs = [
 export const origin = "cna";
 
 const uniqueLinks = [];
-const news = [];
 
-function singleRequest(url) {
-  var req = request(`${baseURL}${url}`)
-    , feedparser = new FeedParser();
+function singleRequest(url, idx) {
+  return new Promise(function(resolve, reject) {
+    const news = [];
+    var req = request(`${baseURL}${url}`)
+      , feedparser = new FeedParser();
 
-  req.on('response', function (res) {
-    var stream = this;
+    req.on('response', function (res) {
+      var stream = this;
 
-    if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+      if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
 
-    stream.pipe(feedparser);
-  });
+      stream.pipe(feedparser);
+    });
 
-  feedparser.on('readable', function() {
-    // This is where the action is!
-    var stream = this
-      , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-      , item;
+    feedparser.on('readable', function() {
+      // This is where the action is!
+      var stream = this
+        , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+        , item;
 
-    while (item = stream.read()) {
-      const { origlink, title, pubDate, description, summary, guid } = item;
-      const link = origlink.replace(/\/$/, "");
-      if (uniqueLinks.indexOf(link) > -1) continue;
-      uniqueLinks.push(link);
+      while (item = stream.read()) {
+        const { origlink, title, pubDate, description, summary, guid } = item;
+        const link = origlink.replace(/\/$/, "");
+        if (uniqueLinks.indexOf(link) > -1) continue;
+        uniqueLinks.push(link);
 
-      news.push({
-        category: meta.title,
-        origin,
-        link,
-        title,
-        pubDate,
-        content: summary,
-        contentSnippet: description,
-        guid,
-      })
-    }
-
-    if (url.match(/video$/)) createJob(news);
+        news.push({
+          category: meta.title,
+          origin,
+          link,
+          title,
+          pubDate,
+          content: summary,
+          contentSnippet: description,
+          guid,
+        })
+      }
+    });
+    feedparser.on('end', function(err) {
+      if (err) return reject(err);
+      resolve(news);
+    });
   });
 }
 
-URLs.map(singleRequest);
+
+Promise.all(URLs.map(singleRequest)).then(results => {
+  const news = results.reduce((acc, cur) => {
+    acc = acc.concat(cur);
+    return acc;
+  } ,[]);
+  createJob(news);
+});

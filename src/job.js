41ca6404e730458f0ed4593f6fbf849665f8dd0f
Ly Cheng <lyforever62@hotmail.com>
2016-09-16 21:20:32 +0800

    Remove async/await

--- src/job.js
+++ src/job.js
@@ -2,13 +2,11 @@ import Queue from 'rethinkdb-job-queue';
 import { jobOptions } from './config/db';
 const q = new Queue(jobOptions);
 
-export async function createJob(news) {
+export function createJob(news) {
 
   const jobs = q.createJob(news.length);
   jobs.map((it, idx) => it.data = news[idx]);
-  const savedJobs = await q.addJob(jobs);
-
-  q.stop();
+  q.addJob(jobs).then(() => q.stop());
 }
 
 export default q;

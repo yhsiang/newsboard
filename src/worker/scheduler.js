62ac7be69d23b5ed76ae0a7f77579e1ddb942adc
Ly Cheng <lyforever62@hotmail.com>
2016-09-17 00:26:28 +0800

    Fixed schedule range

--- src/worker/scheduler.js
+++ src/worker/scheduler.js
@@ -6,9 +6,11 @@ var connection;
 r.connect(dbOptions)
  .then(conn => {
    connection = conn;
-   return r.table(tableName).filter(news =>
-     news("date").gt(r.now().date().sub(187200)) && news("date").lt(r.now().date().sub(7200))
-   ).run(conn);
+   return r
+    .table(tableName)
+    .filter(news => news("date").gt(r.now().date().sub(216000)))
+    .filter(news => news("date").lt(r.now().date().sub(36000)))
+    .run(conn);
  })
 .then(cur => cur.toArray())
 .then(results => createJob(results))

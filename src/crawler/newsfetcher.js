dbaf9c57c6d003bb5e1f88d2878382a59797d79a
Ly Cheng <lyforever62@hotmail.com>
2016-09-17 00:26:11 +0800

    Add cna and ltn, fixed link

--- src/crawler/newsfetcher.js
+++ src/crawler/newsfetcher.js
@@ -31,7 +31,7 @@ function fetch(url) {
       }
 
       results.push({
-        id: link.replace(/\/?\/$/, ''),
+        id: link.replace(/\/\/$/, '/'),
         title,
         date,
       });
@@ -45,13 +45,41 @@ function fetch(url) {
 }
 
 const urls = [
-  'http://www.appledaily.com.tw/rss/newcreate/kind/rnews/type/new',
-  'http://www.chinatimes.com/rss/realtimenews.xml',
-  'http://feeds.feedburner.com/ettoday/realtime',
-  'http://www.peoplenews.tw/rss/%E7%B8%BD%E8%A6%BD',
-  'http://www.storm.mg/feeds/all',
-  'http://feeds.initium.news/theinitium',
-  'http://udn.com/rssfeed/news/1',
+  "http://www.appledaily.com.tw/rss/newcreate/kind/rnews/type/new",
+  "http://www.chinatimes.com/rss/realtimenews.xml",
+  "http://feeds.feedburner.com/ettoday/realtime",
+  "http://www.peoplenews.tw/rss/%E7%B8%BD%E8%A6%BD",
+  "http://www.storm.mg/feeds/all",
+  "http://feeds.initium.news/theinitium",
+  "http://udn.com/rssfeed/news/1",
+  "http://feeds.feedburner.com/rsscna/PhotoAlbum",
+  "http://feeds.feedburner.com/rsscna/politics",
+  "http://feeds.feedburner.com/rsscna/social",
+  "http://feeds.feedburner.com/rsscna/lifehealth",
+  "http://feeds.feedburner.com/rsscna/finance",
+  "http://feeds.feedburner.com/rsscna/intworld",
+  "http://feeds.feedburner.com/rsscna/mainland",
+  "http://feeds.feedburner.com/rsscna/stars",
+  "http://feeds.feedburner.com/rsscna/sport",
+  "http://feeds.feedburner.com/rsscna/local",
+  "http://feeds.feedburner.com/rsscna/TopNewsforWeek",
+  "http://feeds.feedburner.com/rsscna/video",
+  "http://news.ltn.com.tw/rss/focus.xml",
+  "http://news.ltn.com.tw/rss/politics.xml",
+  "http://news.ltn.com.tw/rss/society.xml",
+  "http://news.ltn.com.tw/rss/life.xml",
+  "http://news.ltn.com.tw/rss/opinion.xml",
+  "http://news.ltn.com.tw/rss/world.xml",
+  "http://news.ltn.com.tw/rss/business.xml",
+  "http://news.ltn.com.tw/rss/sports.xml",
+  "http://news.ltn.com.tw/rss/entertainment.xml",
+  "http://news.ltn.com.tw/rss/consumer.xml",
+  "http://news.ltn.com.tw/rss/supplement.xml",
+  "http://news.ltn.com.tw/rss/local.xml",
+  "http://news.ltn.com.tw/rss/taipei.xml",
+  "http://news.ltn.com.tw/rss/northern.xml",
+  "http://news.ltn.com.tw/rss/central.xml",
+  "http://news.ltn.com.tw/rss/southern.xml",
 ];
 
 var connection;

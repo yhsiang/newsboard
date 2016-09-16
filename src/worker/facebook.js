347ced7491a20337f2ac8a2ad5c491a7328f1de2
Ly Cheng <lyforever62@hotmail.com>
2016-09-16 21:37:58 +0800

    Remove async/await in facebook worker

--- src/worker/facebook.js
+++ src/worker/facebook.js
@@ -15,32 +15,33 @@ const q = new Queue(jobOptions);
 q.on('completed', jobId => console.log(`${new Date()} Job completed: ${jobId}`));
 q.on('failed', jobId => console.log(`${new Date()} Job Failed: ${jobId}`));
 
-async function processJob() {
-  const conn = await r.connect(dbOptions);
-  const token = await API('oauth/access_token', authOptions);
-  fb.setAccessToken(token);
-  console.log('Auth done!');
-
-  q.process(async (job, next) => {
-    const graph = await API(job.data.link);
-    if (graph.share) {
-      const { id, share, og_object } = graph;
-      const { id: ogId, ...rest } = og_object;
-      const result = await r
-        .table(tableName)
-        .insert({
-          link: id,
-          ogId,
-          ...rest,
-          ...share,
-        })
-        .run(conn);
-      setTimeout(() => next('Insert to db'), 1000);
-    } else {
-      const err = new Error('No Graph Data');
-      next(err);
-    }
-  });
-}
-
-processJob();
+var connection;
+r.connect(dbOptions)
+ .then(conn => API('oauth/access_token', authOptions))
+ .then(token => {
+   fb.setAccessToken(token);
+   console.log('Auth done!');
+   q.process((job, next) => {
+     API(job.data.link)
+       .then(graph => {
+         if (graph.error) {
+           return next(new Error(graph.error.message));
+         }
+         if (!graph.share) {
+           return next(new Error('No Graph Data'));
+         }
+         const { id, share, og_object } = graph;
+         const { id: ogId, ...rest } = og_object;
+         const result = await r
+           .table(tableName)
+           .insert({
+             link: id,
+             ogId,
+             ...rest,
+             ...share,
+           })
+           .run(conn);
+         setTimeout(() => next('Insert to db'), 1000);
+     });
+   });
+ });

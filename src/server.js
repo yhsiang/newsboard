af4925bd79a7f41e4da6a897c608d8936a160f35
Ly Cheng <lyforever62@hotmail.com>
2016-09-17 14:27:53 +0800

    Use ejs to be template

--- src/server.js
+++ src/server.js
@@ -1,31 +1,14 @@
 import express from "express";
 import r from "rethinkdb";
 import { dbOptions } from "./config/db";
+import moment from "moment";
 
 const PORT = process.env.PORT || 5240;
 const app = express();
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send("I am newsboard API"));
-
-app.get('/share', (req, res) => {
-  var connection;
-  r.connect(dbOptions)
-   .then(conn => {
-     connection = conn;
-     return r
-      .table("Graph")
-      .orderBy(r.desc("share_count"))
-      .limit(10)
-      .run(connection);
-   })
-   .then(results => {
-     connection.close();
-     return res.json(results);
-   });
-});
-
-app.get('/comment', (req, res) => {
-  var connection;
+app.get('/', (req, res) => {
+  var connection, comments;
   r.connect(dbOptions)
    .then(conn => {
      connection = conn;
@@ -36,9 +19,22 @@ app.get('/comment', (req, res) => {
       .run(connection);
    })
    .then(results => {
-     connection.close();
-     return res.json(results);
-   });
+     comments = results;
+     return r
+      .table("Graph")
+      .orderBy(r.desc("share_count"))
+      .limit(10)
+      .run(connection);
+   })
+   .then(results => {
+     res.render("index.ejs", {
+       title: `${moment().format("MM-DD")} 排行`,
+       shares: results,
+       comments,
+       moment,
+     });
+   })
+
 });
 
 app.listen(PORT, () => console.log(`Server listen at ${PORT}`));

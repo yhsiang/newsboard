14790a838fda04d78c4bea6dc8914a9dc0f6cb1a
Ly Cheng <lyforever62@hotmail.com>
2016-09-19 02:06:30 +0800

    Add admin and seperate 24 and 48 results

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
      connection = conn;
      return r
       .table("Graph")
+      .filter(news => news("date").gt(r.now().date().sub(86400)))
       .orderBy(r.desc("comment_count"))
       .limit(10)
       .run(connection);
@@ -22,19 +23,68 @@ app.get('/', (req, res) => {
      comments = results;
      return r
       .table("Graph")
+      .filter(news => news("date").gt(r.now().date().sub(86400)))
       .orderBy(r.desc("share_count"))
       .limit(10)
       .run(connection);
    })
    .then(results => {
      res.render("index.ejs", {
-       title: `${moment().format("MM/DD/YYYY")} 熱門新聞整理`,
+       title: `${moment().format("MM/DD/YYYY")} 24 小時內熱門新聞整理`,
        shares: results,
        comments,
        moment,
      });
    })
+});
 
+app.get('/48', (req, res) => {
+  var connection, comments;
+  r.connect(dbOptions)
+   .then(conn => {
+     connection = conn;
+     return r
+      .table("Graph")
+      .filter(news => news("date").gt(r.now().date().sub(2*86400)))
+      .orderBy(r.desc("comment_count"))
+      .limit(10)
+      .run(connection);
+   })
+   .then(results => {
+     comments = results;
+     return r
+      .table("Graph")
+      .filter(news => news("date").gt(r.now().date().sub(2*86400)))
+      .orderBy(r.desc("share_count"))
+      .limit(10)
+      .run(connection);
+   })
+   .then(results => {
+     res.render("index.ejs", {
+       title: `${moment().format("MM/DD/YYYY")} 48 小時內熱門新聞整理`,
+       shares: results,
+       comments,
+       moment,
+     });
+   })
+});
+
+app.get('/admin', (req, res) => {
+  var connection, comments;
+  r.connect(dbOptions)
+   .then(conn => {
+     connection = conn;
+     return r
+      .table("Graph")
+      .run(connection);
+   })
+   .then(cur => cur.toArray())
+   .then(results => {
+     res.render("admin.ejs", {
+       data: results,
+       moment,
+     });
+   })
 });
 
 app.listen(PORT, () => console.log(`Server listen at ${PORT}`));
